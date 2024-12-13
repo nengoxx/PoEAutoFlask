e1e49c94b11ac9a4163b36f9fc61da0e9161cf86
nengoxx <obitoyelmaki@hotmail.com>
2024-12-13 16:22:00 +0100

    updated for PoE2
    The autohotkey one is for PoE1,probably works if you change the window name but I wont be maintaining it, instead I'll switch to python.
    You can roll after casting/attacking and auto command minions as you hold down left click or use right click skill.
    
    TODO: better overlay & auto flasks

--- PoEAutoFlask_python.py
+++ PoEAutoFlask_python.py
@@ -14,26 +14,45 @@ pyautogui.FAILSAFE = False
 pyautogui.MINIMUM_SLEEP = 0
 pyautogui.MINIMUM_DURATION = 0
 
-botting = False
-SkillSpam = False
-triggerSkill = True
-rightClicked = False
-windowName = "Diablo Immortal"
+botting = False #bot is on
+SkillSpam = False #bot is using skills
+rightClicked = False #Right click pressed
+leftClicked = False
+middleClicked = False
+windowName = "Path of Exile 2"
 
+#Extra functionality for left click holding
+walkCast=False
+press_time = 0
+hold_threshold = 3  # seconds
+
+#Cast after roll
+rollCast=False
+
+#Key list for auto rotation
 keyList=['shift+e','shift+r','shift+2','shift+q']
 
+#Key list for skills
+commandMinions_s="1"
+roll_s="space"
 
+#Toggle buttons
+walkCast_btn='ñ'
+rollCast_btn='0'
+
+#Toggle bot on/off
 def switchBot(kb_event_info):
     global botting
     botting = not botting
     showText()
-    
+
+#Hard stop the bot
 def stopBot(kb_event_info):
     global botting
     botting = False
     showText()
 
-
+#Text to show the bot status
 def showText():
     global botting
     label = tkinter.Label(text='autoF', font=(None, '12', 'bold'), fg='white', bg='black')
@@ -57,38 +76,65 @@ def get_active_window():
 def toggleSkillOn(kb_event_info):
     #print('skill pressed!')
     global SkillSpam
-    SkillSpam = True
+    if not SkillSpam:
+        SkillSpam = True
+        time.sleep(0.05)
     
 def toggleSkillOff(kb_event_info):
     global SkillSpam
-    SkillSpam = False
-    
+    if SkillSpam:
+        SkillSpam = False
+        time.sleep(0.05)
+
+#Extra functionality toggle:
+#Command minions while walking
+def toggleWalkCast(kb_event_info):
+    global walkCast
+    walkCast = not walkCast
+
+#Roll after cast toggle
+def toggleRollCast(kb_event_info):
+    global rollCast
+    rollCast = not rollCast
+
+#Mouse click actions/bindings
 def on_click(x, y, button, pressed):
-    global triggerSkill
-    global rightClicked
+    global rightClicked,leftClicked,middleClicked,walkCast,press_time,hold_threshold
     if pressed:
         #print(button)
+        if walkCast and (button == mouse.Button.left):
+            press_time = time.time()  # Record the time when the button is pressed
+            leftClicked =True
+            threading.Thread(target=check_hold_duration).start()  # Start checking for hold
+            #toggleSkillOn(None)
         if (button == mouse.Button.right):
-            triggerSkill = False
             rightClicked =True
             toggleSkillOn(None)
-            keyboard.press("shift")
-        if (button == mouse.Button.x1) or (button == mouse.Button.x1):
-            # if (button == mouse.Button.x2):
-            #     sleep(randint(50, 150)/1000)
-            #     keyboard.send('q')
-            #     return
+        if (button == mouse.Button.middle):
+            middleClicked =True
             toggleSkillOn(None)
-            return
-        else:
-            return
+        # if (button == mouse.Button.x1) or (button == mouse.Button.x1):
+        #     toggleSkillOn(None)
+        return
     else:
         if rightClicked:
-            keyboard.release("shift")
             rightClicked = False
+        if leftClicked:
+            leftClicked = False
+            press_time = 0
+        if middleClicked:
+            middleClicked = False
         toggleSkillOff(None)
         return
         
+def check_hold_duration():
+    global press_time, leftClicked
+    while leftClicked:  # While the button is still pressed
+        duration = time.time() - press_time
+        if duration >= hold_threshold and leftClicked:
+            toggleSkillOn(None)  # Trigger hold action
+            return  # Exit the loop once the hold action is triggered
+        time.sleep(0.01)  # Check every 10ms to avoid high CPU usage
 
 def main():
     global botting
@@ -97,65 +143,29 @@ def main():
         keyboard.on_press_key('enter',stopBot)
         listener = mouse.Listener(on_click=on_click)
         listener.start()
-        keyboard.on_press_key('e',toggleSkillOn)
-        keyboard.on_release_key('e',toggleSkillOff)
-        keyboard.on_press_key('w',toggleSkillOn)
-        keyboard.on_release_key('w',toggleSkillOff)
+        keyboard.on_press_key(walkCast_btn,toggleWalkCast)
+        keyboard.on_press_key(rollCast_btn,toggleRollCast)
+        # keyboard.on_press_key('w',toggleSkillOn)
+        # keyboard.on_release_key('w',toggleSkillOff)
         while True:
             while (get_active_window()):
                 if botting:
                     if SkillSpam:
-                        #skillThreading2()
-                        #fullRotation()
-                        if not rightClicked:
-                            fullRotation_mage()
-                        # else:
-                        #     basicAttack()
+                        # fullRotation()
+                        #if not rightClicked:
+                        #     fullRotation_mage()
+                        #    continue
+                        #else:
+                        commandMinions() #command only on right click
+                    time.sleep(0.05)
             if not botting:
                 time.sleep(0.1)
     
-    listener.join()
     
     
-def skillThreading():
-    global keyList
-    skillThread = 0
-    x = None
-    while (skillThread < len(keyList)):
-        x = threading.Thread(target=keyThread_send, args=(keyList[skillThread],))
-        x.start()
-        skillThread += 1
-    x.join()
-    
-def keyThread_send(key):
-    sleep(randint(50, 150)/1000)
-    keyboard.send(key)
-    
-def skillThreading2():
-    global keyList
-    skillThread = 0
-    x=None
-    while (skillThread <= len(keyList)/2):
-        #x = threading.Thread(target=keyThread, args=(keyList[skillThread],keyList[skillThread+1]))
-        x = threading.Thread(target=keyThread2_send, args=(skillThread,))
-        x.start()
-        skillThread += 2
-    x.join()
 
-def keyThread2_send(key):
-    global keyList
-    keyboard.send('shift+space')
-    sleep(randint(50, 150)/1000)
-    if not SkillSpam:
-        return
-    keyboard.send(keyList[key])
-    sleep(randint(50, 150)/1000)
-    if not SkillSpam:
-        return
-    keyboard.send(keyList[key+1])
-    sleep(randint(50, 150)/1000)
 
-def skillPress():
+""" def skillPress():
     global triggerSkill
     
     if (not triggerSkill):
@@ -178,10 +188,10 @@ def skillPress():
     sleep(randint(20, 100)/1000)
     keyboard.press("q")
     sleep(randint(10, 50)/1000)
-    keyboard.release("q")    
+    keyboard.release("q")  """   
     
 
-def skillPress_send():
+""" def skillPress_send():
     global triggerSkill
     
     if (not triggerSkill):
@@ -196,10 +206,10 @@ def skillPress_send():
     keyboard.send(keyList[2])
     
     sleep(randint(20, 150)/1000)
-    keyboard.send(keyList[3])                
+    keyboard.send(keyList[3])  """               
                     
                     
-def skillPress_long():
+""" def skillPress_long():
     global triggerSkill
     timeout = time.time() + 2 #2sec
     tries = 0
@@ -230,9 +240,9 @@ def skillPress_long():
         if tries > 1 or time.time() > timeout:
             break
     
-    toggleSkillOff(None)
+    toggleSkillOff(None) """
     
-def fullRotation():
+""" def fullRotation():
     keyboard.send("shift+r")
     sleep(randint(10, 70)/1000)
     if not SkillSpam:
@@ -254,25 +264,20 @@ def fullRotation():
         return
     
     keyboard.send("shift+space")
-    sleep(randint(10, 70)/1000)
+    sleep(randint(10, 70)/1000) """
     
-    
-def fullRotation_mage():
-    
-    keyboard.send("w")
-    sleep(randint(10, 70)/1000)
-    keyboard.send("e")
-    sleep(randint(10, 70)/1000)
-    # keyboard.send("q")
-    # sleep(randint(10, 70)/1000)
-    # keyboard.send("d")
-    # sleep(randint(10, 70)/1000)
-    keyboard.send("shift+space")
-    sleep(randint(10, 70)/1000)
 
-def basicAttack():
-    sleep(randint(20, 150)/1000)
-    keyboard.send("shift+space")
+def commandMinions():
+    global rollCast,leftClicked
+    sleep(randint(150, 250)/1000)
+    keyboard.send(commandMinions_s)
+    #pyautogui.press(commandMinions_s)
+    # keyboard.press(commandMinions_s)
+    # sleep(randint(10, 50)/1000)
+    # keyboard.release(commandMinions_s)
+    if (not leftClicked) & rollCast:
+        sleep(randint(250, 350)/1000)
+        keyboard.send(roll_s)
 
 
 
---
+++ start.bat
@@ -0,0 +1,9 @@
+@echo off
+
+call .\venv\Scripts\activate.bat
+
+
+python PoeAutoFlask_python.py
+
+:: Keep terminal open
+cmd /k
